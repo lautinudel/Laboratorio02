6693a1bd33bd6eef07b039db848cb143ce74ebc3
Lautaro <lautaronudel@gmail.com>
2018-10-21 18:22:50 -0300

    Inciso 2 TP3 Parte 2 terminado (notificaciones)

--- app/src/main/java/ar/edu/utn/frsf/dam/isi/laboratorio02/EstadoPedidoReceiver.java
+++ app/src/main/java/ar/edu/utn/frsf/dam/isi/laboratorio02/EstadoPedidoReceiver.java
@@ -1,8 +1,16 @@
 package ar.edu.utn.frsf.dam.isi.laboratorio02;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.BitmapFactory;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.widget.Toast;
 
 import java.util.List;
@@ -21,9 +29,37 @@ public class EstadoPedidoReceiver extends BroadcastReceiver {
             if(intent.getStringExtra("estado").equals("ESTADO_ACEPTADO")){
                 idABuscar = intent.getIntExtra("idPedido", 0);
                 List<Pedido> lista = repositorioPedido.getLista();
+                String fecha="";
+                int i=0;
+                NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
                 for(Pedido p:lista){
-                    if(p.getId().equals(idABuscar))
-                        Toast.makeText(context,"Pedido para"+p.getMailContacto()+" ha cambiado a estado ACEPTADO",Toast.LENGTH_LONG).show();
+                    if(p.getId().equals(idABuscar)){
+                        fecha=p.getFecha().toString();
+                        //Toast.makeText(context,"Pedido para"+p.getMailContacto()+" ha cambiado a estado ACEPTADO",Toast.LENGTH_LONG).show();
+                        Intent resultIntent = new Intent(context, AltaPedido.class);
+                        resultIntent.putExtra("id", p.getId().toString());
+                        TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+                        stackBuilder.addNextIntentWithParentStack(resultIntent);
+                        PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+
+                        Notification notification = new NotificationCompat.Builder(context, "CANAL01").setContentIntent(resultPendingIntent)
+                                .setSmallIcon(R.mipmap.ic_launcher)
+                                .setContentTitle("Tu pedido fue aceptado")
+                                .setContentText("El costo sera de "+p.total())
+                                .setStyle(new NotificationCompat.InboxStyle()
+                                        .addLine("Previsto el envio para: ")
+                                        .addLine(fecha)
+                                        )
+                                .build();
+
+
+                        notificationManager.notify(i, notification);
+
+                        i++;
+                    }
+
+
+
 
                 }
             }

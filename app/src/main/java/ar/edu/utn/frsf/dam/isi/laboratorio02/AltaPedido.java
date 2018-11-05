6ab383af0f2c942f135c44373fc04cee48a2539a
Lautaro <lautaronudel@gmail.com>
2018-11-04 22:40:13 -0300

    Req01 terminado (ejercicio 1 lab4)

--- app/src/main/java/ar/edu/utn/frsf/dam/isi/laboratorio02/AltaPedido.java
+++ app/src/main/java/ar/edu/utn/frsf/dam/isi/laboratorio02/AltaPedido.java
@@ -2,6 +2,7 @@ package ar.edu.utn.frsf.dam.isi.laboratorio02;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -48,9 +49,7 @@ public class AltaPedido extends AppCompatActivity {
         setContentView(R.layout.activity_alta_pedido);
         intent = getIntent();
 
-        SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("configuracion_ui",0);
-        System.out.println("PREFERENCIAS: "+sharedPreferences.getString("edit_text_preference_1","Default value"));
-
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);//getApplicationContext().getSharedPreferences("Preferencias",0);
 
         Button btnPedidoAddProducto = (Button) findViewById(R.id.btnPedidoAddProducto);
         Button btnPedidoVolver = (Button) findViewById(R.id.btnPedidoVolver);
@@ -65,7 +64,8 @@ public class AltaPedido extends AppCompatActivity {
         lista = (ListView) findViewById(R.id.lstPedidoItems);
         final EditText edtPedidoCorreo = (EditText) findViewById(R.id.edtPedidoCorreo);
         final EditText edtPedidoHoraEntrega = (EditText) findViewById(R.id.edtPedidoHoraEntrega);
-
+        edtPedidoCorreo.setText(sharedPreferences.getString("edit_text_preference_1","Default value"));
+        optPedidoRetira.setChecked(sharedPreferences.getBoolean("check_box_preference_1", false));
         //Si vengo de historial de pedidos (detalles)
         int idABuscar = -1;
         if(getIntent().getExtras()!=null){
--- app/src/main/java/ar/edu/utn/frsf/dam/isi/laboratorio02/Preferencias.java
+++ app/src/main/java/ar/edu/utn/frsf/dam/isi/laboratorio02/Preferencias.java
@@ -1,8 +1,13 @@
 package ar.edu.utn.frsf.dam.isi.laboratorio02;
 
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.preference.PreferenceFragmentCompat;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
 
 import ar.edu.utn.frsf.dam.isi.laboratorio02.R;
 
@@ -23,6 +28,8 @@ public class Preferencias extends AppCompatActivity {
                 .beginTransaction()
                 .replace(android.R.id.content, new ConfiguracionFragment())
                 .commit();
+
     }
+
 
 }
